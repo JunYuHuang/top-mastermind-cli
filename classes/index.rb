99c44d2c9b94eeb693afba1960dbfa3cbc742293
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-05-12 22:09:39 -0700

    Add 11 completed and incomplete methods to Game class

--- classes/index.rb
+++ classes/index.rb
@@ -1,4 +1,5 @@
 require 'set'
+require 'io/console'
 
 class Game
   def initialize(player_maker_class, player_breaker_class)
@@ -14,7 +15,7 @@ class Game
     ]
     @guesses = []
     @secret_code = []
-    @feedback = []
+    @responses = []
   end
 
   attr_reader(:choices)
@@ -23,7 +24,7 @@ class Game
   attr_reader(:players)
   attr_accessor(:guesses)
   attr_accessor(:secret_code)
-  attr_accessor(:feedback)
+  attr_accessor(:responses)
 
   def play
     # TODO
@@ -56,8 +57,73 @@ class Game
     true
   end
 
-  def get_feedback(guess)
+  # TODO: method incomplete
+  def get_response(guess)
+    res = { :correct => 0, :misplaced => 0 }
+    correct_indices = Set.new
+    guess.each_with_index do |value, i|
+      if value == @secret_code[i]
+        res[:correct] += 1
+        correct_indices.add(i)
+      end
+    end
+    # misplaced_choices = Set.new([])
+  end
+
+  # converts string `guess` to int array so it can be
+  # easily compared against @secret_code
+  def parse_guess(guess)
+    res = []
+    guess.each do |char|
+      res.push(char.to_i)
+    end
+    res
+  end
+
+  def did_maker_win?
+    @guesses.size > @max_guesses
+  end
+
+  def did_breaker_win?(code)
+    code == @secret_code
+  end
+
+  def update_game(guess, response)
+    @guesses.push(guess)
+    @responses.push(response)
+  end
+
+  def clear_console
+    $stdout.clear_screen
+  end
+
+  def print_board
     # TODO
+  end
+
+  def print_breaker_prompt
+    res = [
+      "The secret code is a #{@code_length} sequence\n",
+      "of any combination of the below choices\n",
+      "that may contain duplicates\n",
+      "\n",
+      "Possible choices#{@choices.to_a}\n",
+      "Guess attempts left: #{@max_guesses - @guesses.size}\n",
+      "Enter your guess (no spaces):"
+    ]
+    puts(res.join)
+  end
+
+  def print_invalid_input_message(input)
+    puts("\n#{input} is not a valid guess. Try again.")
+  end
+
+  def print_breaker_won
+    puts("\nGame ended: Code Breaker(#{get_player_breaker.to_s}) won!")
+  end
+
+  def print_maker_won
+    puts("\nGame ended: Code Maker(#{get_player_maker.to_s}) won!")
   end
 end
 
